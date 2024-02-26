ec4215dd0380dd40f902576244c808753666b024
Mubashir-Sohail <158273043+Mubashir-Sohail@users.noreply.github.com>
2024-02-26 07:21:52 +0000

    perform operation

---
+++ 01_basics/operation.js
@@ -0,0 +1,24 @@
+// operation
+
+let num= 33
+let num1= -num
+console.log(num1);
+console.log(typeof num1);
+
+// console.log(2*2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%3);    reminder ka liya
+// console.log(2**2;    power ka liya
+
+let a= "hello"
+let b= " gii"
+let c=a+b;                  // concate kar diya haa 
+console.log(c)
+
+// prefix , postfix
+
+let number=100
+++ number;
+console.log()
