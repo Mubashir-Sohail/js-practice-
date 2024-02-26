bc54e62275f62cf9116290fae44ce68b474cd464
Mubashir-Sohail <158273043+Mubashir-Sohail@users.noreply.github.com>
2024-02-26 06:31:39 +0000

    conversion of datatype

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,23 @@
-console.log("hello gii")
+let num=33
+console.log(typeof num);
+
+let n="32abc"
+console.log(typeof n);
+
+
+// number ki hamashah value (NaN) aya gi agar ham (34abc ) if ham number ka sath koi alphabet likha
+let numberIn =Number(n)
+console.log ( numberIn);
+
+let num1 = null
+console.log(typeof num1);
+
+// notes:     "33" => 33
+//          "33abc"=> NaN           type is always (number)
+//           true  => 1 ,   fasle => 0
+
+let loggin = ""
+ let numberbool =Boolean(loggin)
+ console.log(numberbool);
+
+ //  "" => fasle  ,    "alishba" => true
