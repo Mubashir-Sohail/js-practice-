5b6cc5edef1f74ecc0caf61d8c92f0637392a025
Mubashir-Sohail <158273043+Mubashir-Sohail@users.noreply.github.com>
2024-02-26 07:36:23 +0000

    comparison.js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,23 @@
+/*  console.log(2>1)
+    console.log(2<1)
+    console.log(2==1)
+    console.log(2 !=1)     
+
+console.log("2">1)
+console.log("2"<1)
+console.log("2"==1)    
+
+
+console.log( null >0)
+console.log( null <0)
+console.log( null >=0)
+
+console.log( undefined >0)
+console.log( undefined <0)
+console.log( undefined >=0)    */
+
+
+// == ya srif comparison kartha ha only
+// === ya comparison bhi kartha ha or datatype bhi check kartha ha
+ 
+console.log("2">==0)
